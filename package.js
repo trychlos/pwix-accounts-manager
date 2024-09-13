15beb6887218e52e8772e7958126af3d9d38a4d6
Pierre Wieser <p.wieser@trychlos.org>
2024-09-13 12:59:46 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:accounts-manager',
-    version: '1.1.1',
+    version: '1.1.2-rc',
     summary: 'Provide the most common parts of a simple accounts management system',
     git: 'https://github.com/trychlos/pwix-accounts-manager',
     documentation: 'README.md'
